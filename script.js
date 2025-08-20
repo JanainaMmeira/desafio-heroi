be529f549a156de2c69719e8b664fcdcfa961d94
JanainaMmeira <janainamcamara@hotmail.com>
2025-08-20 18:11:38 -0300

    Alterando imagens dos personagens

--- script.js
+++ script.js
@@ -1,55 +1,76 @@
 function classificarHeroi() {
-    const nome = document.getElementById('nome').value;
-    const xp = parseInt(document.getElementById('xp').value);
-    let nivel;
+    const nomeInput = document.getElementById('nome');
+    const xpInput = document.getElementById('xp');
+    const nome = nomeInput.value || "Herói";
+    const xp = parseInt(xpInput.value);
+    const section = document.querySelector(".superior");
+
+     let nivel = '';
+    let imagembk = '';
 
     if (xp < 1000) {
         nivel = "Ferro";
+        imagembk = './assets/ferro.jpg';
     } else if (xp >= 1001 && xp <= 2000) {
         nivel = "Cristal";
+        imagembk = './assets/cristal.jpg';
     } else if (xp >= 2001 && xp <= 3000) {
         nivel = "Bronze";
+        imagembk = './assets/bronze.jpg';
     } else if (xp >= 3001 && xp <= 4000) {
         nivel = "Prata";
+        imagembk = './assets/prata.jpg';
     } else if (xp >= 4001 && xp <= 5000) {
         nivel = "Ouro";
+        imagembk = './assets/ouro.jpg';
     } else if (xp >= 5001 && xp <= 6000) {
         nivel = "Platina";
+        imagembk = './assets/platina.jpg';
     } else if (xp >= 6001 && xp <= 7000) {
         nivel = "Diamante";
+        imagembk = './assets/diamante.jpg';
     } else if (xp >= 7001 && xp <= 8000) {
         nivel = "Mestre";
+        imagembk = './assets/mestre.jpg';
     } else if (xp >= 8001 && xp <= 9000) {
         nivel = "Ascendente";
+        imagembk = './assets/ascendente.jpg';
     } else if (xp >= 9001 && xp <= 10000) {
         nivel = "Imortal";
+        imagembk = './assets/imortal.jpg';
     } else if (xp >=10001) {
         nivel = "Radiante";
+        imagembk = './assets/sem_nome.png';
     }
 
     else {
         nivel = " desconhecido, informe os dados acima ↑";
+        imagembk = './assets/unknown.png';
+    }
+ // Atualiza resultado
+    document.getElementById('resultado').innerText = `O Herói ${nome} está no nível ${nivel}.`;
+
+    // Troca a imagem do section.superior
+    if (imagembk !== "") {
+        section.style.backgroundImage = `url('${imagembk}')`;
     }
 
-    document.getElementById('resultado').innerText = ` O Herói ${nome} está no nível ${nivel}.`;
-    limparCampo();
+    // Limpa os campos
+    nomeInput.value = '';
+    xpInput.value = '';
 }
 
-document.getElementById('nome').addEventListener('keydown', function (event) {
+// Eventos de Enter nos inputs
+document.getElementById('nome').addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
         classificarHeroi();
         event.preventDefault();
     }
 });
 
-document.getElementById('xp').addEventListener('keydown', function (event) {
+document.getElementById('xp').addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
         classificarHeroi();
         event.preventDefault();
     }
 });
-
-function limparCampo() {
-    nome.value = '';
-    xp.value = '';
-}
